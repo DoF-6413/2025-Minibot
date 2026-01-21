c2ca30ef17a76ed0400922c201edb92cd4e21519
Lohit Paruchuri <Lohit.Paruchuri@sisepuedefoundation.org>
2026-01-20 19:18:45 -0700

    WIP: started importing the project to 2026 wpilib

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,8 @@ package frc.robot;
 import com.ctre.phoenix6.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.swerve.SwerveModuleConstants.DriveMotorArrangement;
 import com.ctre.phoenix6.swerve.SwerveModuleConstants.SteerMotorArrangement;
+
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.generated.TunerConstants;
@@ -73,6 +75,9 @@ public class Robot extends LoggedRobot {
 
     // Start AdvantageKit logger
     Logger.start();
+
+    // Camera
+    CameraServer.startAutomaticCapture();
 
     // Check for valid swerve config
     var modules =
