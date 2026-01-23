45c3c184a114fc2bd664fcdb9e5932b871fee9ca
Lohit Paruchuri <Lohit.Paruchuri@sisepuedefoundation.org>
2026-01-22 18:25:51 -0700

    Got the code working with the 2026 version of WPILib and also added a camera feed

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Minibot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 12;
-  public static final String GIT_SHA = "c2ca30ef17a76ed0400922c201edb92cd4e21519";
-  public static final String GIT_DATE = "2026-01-20 21:18:45 EST";
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "d651d7f4c460c916aee941c4a1f2a29ff046ca71";
+  public static final String GIT_DATE = "2026-01-20 21:48:26 EST";
   public static final String GIT_BRANCH = "2026_version";
-  public static final String BUILD_DATE = "2026-01-20 21:41:44 EST";
-  public static final long BUILD_UNIX_TIME = 1768963304105L;
+  public static final String BUILD_DATE = "2026-01-22 20:13:14 EST";
+  public static final long BUILD_UNIX_TIME = 1769130794003L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -76,11 +76,11 @@ public class TunerConstants {
 
   // CAN bus that the devices are located on;
   // All swerve devices must share the same CAN bus
-  public static final CANBus kCANBus = new CANBus("canivore", "./logs/example.hoot");
+  public static final CANBus kCANBus = new CANBus("Drivetrain", "./logs/example.hoot");
 
   // Theoretical free speed (m/s) at 12 V applied output;
   // This needs to be tuned to your individual robot
-  public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(5.04);
+  public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(10);
 
   // Every 1 rotation of the azimuth results in kCoupleRatio drive motor turns;
   // This may need to be tuned to your individual robot
