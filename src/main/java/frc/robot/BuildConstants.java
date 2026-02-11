4e6673f93ecf18d4a7c3f9ab2e210e858fd286af
Quinn Phillips <Quinn.Phillips@sisepuedefoundation.org>
2026-02-10 19:17:59 -0700

    added_pathplanner_autos

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Minibot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 16;
-  public static final String GIT_SHA = "2bdf05fef28cf7b122bede12699e81c393e7867d";
-  public static final String GIT_DATE = "2026-02-09 19:34:10 EST";
+  public static final int GIT_REVISION = 17;
+  public static final String GIT_SHA = "47e7d97e732acd650f74306e0467e9ede85ad776";
+  public static final String GIT_DATE = "2026-02-09 21:39:57 EST";
   public static final String GIT_BRANCH = "Shooter";
-  public static final String BUILD_DATE = "2026-02-09 21:36:15 EST";
-  public static final long BUILD_UNIX_TIME = 1770690975775L;
+  public static final String BUILD_DATE = "2026-02-10 21:09:16 EST";
+  public static final long BUILD_UNIX_TIME = 1770775756330L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
