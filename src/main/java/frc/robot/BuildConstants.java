71011e30846f99af7c828c631c775dc9d26e041e
Lohit Paruchuri <Lohit.Paruchuri@firewheel.org>
2026-08-25 20:04:07 -0700

    untested code that tries to limit the drive jittering

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Minibot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 14;
-  public static final String GIT_SHA = "cfb67bb665ce87a058c908991881cdbeabb59a16";
-  public static final String GIT_DATE = "2026-08-20 22:53:31 EDT";
+  public static final int GIT_REVISION = 15;
+  public static final String GIT_SHA = "8c4c76751d4df4d1102b2b55348b040fc7a0d53c";
+  public static final String GIT_DATE = "2026-08-23 01:35:05 EDT";
   public static final String GIT_BRANCH = "drive";
-  public static final String BUILD_DATE = "2026-08-23 01:33:51 EDT";
-  public static final long BUILD_UNIX_TIME = 1787463231725L;
+  public static final String BUILD_DATE = "2026-08-25 22:06:35 EDT";
+  public static final long BUILD_UNIX_TIME = 1787709995883L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
