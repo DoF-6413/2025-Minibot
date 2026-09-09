808b587033311939bb3a90a9da728139051a3d35
Bruce Kahn <DontSpamBruce@POBox.com>
2026-09-08 23:07:28 -0700

    Add Module

---
+++ src/main/java/frc/robot/subsystems/drive/Module.java
@@ -0,0 +1,106 @@
+package frc.robot.subsystems.drive;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
+import org.littletonrobotics.junction.Logger;
+
+/** Wraps a single swerve module's IO layer with kinematics-aware helpers used by {@link Drive}. */
+public class Module {
+  private final ModuleIO io;
+  private final ModuleIOInputsAutoLogged inputs = new ModuleIOInputsAutoLogged();
+  private final int index;
+
+  private final Alert driveDisconnectedAlert;
+  private final Alert turnDisconnectedAlert;
+  private final Alert turnEncoderDisconnectedAlert;
+  private SwerveModulePosition[] odometryPositions = new SwerveModulePosition[] {};
+
+  public Module(ModuleIO io, int index) {
+    this.io = io;
+    this.index = index;
+    driveDisconnectedAlert =
+        new Alert("Disconnected drive motor on module " + index + ".", AlertType.kError);
+    turnDisconnectedAlert =
+        new Alert("Disconnected turn motor on module " + index + ".", AlertType.kError);
+    turnEncoderDisconnectedAlert =
+        new Alert("Disconnected turn encoder on module " + index + ".", AlertType.kError);
+  }
+
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Drive/Module" + index, inputs);
+
+    int sampleCount = inputs.odometryTimestamps.length;
+    odometryPositions = new SwerveModulePosition[sampleCount];
+    for (int i = 0; i < sampleCount; i++) {
+      double positionMeters =
+          inputs.odometryDrivePositionsRad[i] * DriveConstants.kWheelRadiusMeters;
+      Rotation2d angle = inputs.odometryTurnPositions[i];
+      odometryPositions[i] = new SwerveModulePosition(positionMeters, angle);
+    }
+
+    driveDisconnectedAlert.set(!inputs.driveConnected);
+    turnDisconnectedAlert.set(!inputs.turnConnected);
+    turnEncoderDisconnectedAlert.set(!inputs.turnEncoderConnected);
+  }
+
+  /** Runs the module with the specified setpoint state. Mutates the state to optimize it. */
+  public void runSetpoint(SwerveModuleState state) {
+    state.optimize(getAngle());
+    state.cosineScale(inputs.turnPosition);
+
+    io.setDriveVelocity(state.speedMetersPerSecond / DriveConstants.kWheelRadiusMeters);
+    io.setTurnPosition(state.angle);
+  }
+
+  /** Runs the module with the specified output while controlling to zero degrees. */
+  public void runCharacterization(double output) {
+    io.setDriveOpenLoop(output);
+    io.setTurnPosition(Rotation2d.kZero);
+  }
+
+  public void stop() {
+    io.setDriveOpenLoop(0.0);
+    io.setTurnOpenLoop(0.0);
+  }
+
+  public Rotation2d getAngle() {
+    return inputs.turnPosition;
+  }
+
+  public double getPositionMeters() {
+    return inputs.drivePositionRad * DriveConstants.kWheelRadiusMeters;
+  }
+
+  public double getVelocityMetersPerSec() {
+    return inputs.driveVelocityRadPerSec * DriveConstants.kWheelRadiusMeters;
+  }
+
+  public SwerveModulePosition getPosition() {
+    return new SwerveModulePosition(getPositionMeters(), getAngle());
+  }
+
+  public SwerveModuleState getState() {
+    return new SwerveModuleState(getVelocityMetersPerSec(), getAngle());
+  }
+
+  public SwerveModulePosition[] getOdometryPositions() {
+    return odometryPositions;
+  }
+
+  public double[] getOdometryTimestamps() {
+    return inputs.odometryTimestamps;
+  }
+
+  public double getWheelRadiusCharacterizationPosition() {
+    return inputs.drivePositionRad;
+  }
+
+  public double getFFCharacterizationVelocity() {
+    return Units.radiansToRotations(inputs.driveVelocityRadPerSec);
+  }
+}
