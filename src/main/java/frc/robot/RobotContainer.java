d1f36d143a052f210b4bef718e094e3212ab718a
Bruce Kahn <DontSpamBruce@POBox.com>
2026-09-08 22:54:02 -0700

    Bootstrap AdvantageKit swerve project skeleton

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class RobotContainer {
+  public RobotContainer() {}
+
+  public Command getAutonomousCommand() {
+    return null;
+  }
+}
